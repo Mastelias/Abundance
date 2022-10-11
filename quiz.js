63fe2e6e3e253fb0f32530f971f88608b8c1e047
Reza-lions <115576168+Reza-lions@users.noreply.github.com>
2022-10-11 21:55:04 +0300

    Test for OnClick

--- quiz.js
+++ quiz.js
@@ -1,2 +1,8 @@
-document.getElementsByClassName("radio input");
-element.setAttribute("class", "democlass");
+let radioButtons = document.getElementsByClassName("Radio Input");
+for (let i=0; i<radioButtons.length; i++) {
+    element[i].setAttribute("input", "alert("This is Working");");
+}
+
+
+
+
