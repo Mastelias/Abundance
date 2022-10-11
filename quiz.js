2a903df67e2c176a52b838da586dcaf4ff37827e
Reza-lions <115576168+Reza-lions@users.noreply.github.com>
2022-10-11 22:09:35 +0300

    Fix

--- quiz.js
+++ quiz.js
@@ -1,6 +1,6 @@
 let radioButtons = document.getElementsByTagName("input");
 for (let i=0; i<radioButtons.length; i++) {
-    element[i].setAttribute("onclick", "alert("This is Working");");
+    element[i].setAttribute("onclick", 'alert("This is Working");');
 }
 
 
